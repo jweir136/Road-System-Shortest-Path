36e1c6c160c2d3d0f1977121457c17d75fbd01d1
jweir136 <jweir136@yahoo.com>
2021-12-22 15:03:14 -0500

    add first draft of constructor

--- src/road_graph.py
+++ src/road_graph.py
@@ -1,5 +1,30 @@
+import csv
 
 class Edge:
-    source:     str
-    end:        str
-    distance:   float
+    def __init__(self, start: str, end: str, distance: float):
+        self.start = start
+        self.end = end
+        self.distance = distance
+
+    def __repr__(self):
+        return "[start: {}, end: {}, distance: {}]".format(self.start, self.end, self.distance)
+
+class RoadwayGraph:
+    def __init__(self, csv_filepath: str):
+        self.adjDict: dict(str, list(Edge)) = {}
+
+        with open(csv_filepath, 'r') as csvfile:
+            datareader = csv.reader(csvfile)
+            for row in datareader:
+                x, y, start, end, node_id, distance = row
+
+                if not start in self.adjDict:
+                    self.adjDict[start] = [Edge(start, end, distance)]
+                else:
+                    self.adjDict[start].append(Edge(start, end, distance))
+
+        
+
+if __name__ == "__main__":
+    r = RoadwayGraph("test-data/Karachi/Karachi_Edgelist.csv")
+    print(r.adjDict)
