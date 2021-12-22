fa8b631d535f22ea74ad3175cfb4b8da0811e22c
jweir136 <jweir136@yahoo.com>
2021-12-22 15:14:45 -0500

    add first draft of add_road

--- src/road_graph.py
+++ src/road_graph.py
@@ -23,7 +23,17 @@ class RoadwayGraph:
                 else:
                     self.adjDict[start].append(Edge(start, end, distance))
 
-        
+    def add_road(self, start: str, end: str, distance: float, one_way=False):
+        if not start in self.adjDict:
+            self.adjDict[start] = [Edge(start, end, distance)]
+        else:
+            self.adjDict[start].append(Edge(start, end, distance))
+
+        if one_way == False:
+            if not end in self.adjDict:
+                self.adjDict[end] = [Edge(end, start, distance)]
+            else:
+                self.adjDict[end].append(Edge(end, start, distance))
 
 if __name__ == "__main__":
     r = RoadwayGraph("test-data/Karachi/Karachi_Edgelist.csv")
