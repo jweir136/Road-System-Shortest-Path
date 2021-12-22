da6dd43b9e67b3a824adb622545089f7750b828a
jweir136 <jweir136@yahoo.com>
2021-12-22 15:57:19 -0500

    add coords

--- src/road_graph.py
+++ src/road_graph.py
@@ -4,7 +4,7 @@ class Edge:
     def __init__(self, start: str, end: str, distance: float):
         self.start = start
         self.end = end
-        self.distance = distance
+        self.distance = float(distance)
 
     def __repr__(self):
         return "[start: {}, end: {}, distance: {}]".format(self.start, self.end, self.distance)
@@ -12,6 +12,7 @@ class Edge:
 class RoadwayGraph:
     def __init__(self, csv_filepath: str):
         self.adjDict: dict(str, list(Edge)) = {}
+        self.coords = {}
 
         with open(csv_filepath, 'r') as csvfile:
             datareader = csv.reader(csvfile)
@@ -22,6 +23,9 @@ class RoadwayGraph:
                     self.adjDict[start] = [Edge(start, end, distance)]
                 else:
                     self.adjDict[start].append(Edge(start, end, distance))
+
+                if not start in self.coords:
+                    self.coords[start] = (x, y)
 
     def add_road(self, start: str, end: str, distance: float, one_way=False):
         if not start in self.adjDict:
@@ -37,4 +41,4 @@ class RoadwayGraph:
 
 if __name__ == "__main__":
     r = RoadwayGraph("test-data/Karachi/Karachi_Edgelist.csv")
-    print(r.adjDict)
+    print(r.coords)
